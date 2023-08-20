1430ec684861a1b7798630cd5a6fa89d85abb523
김재형 <127040847+Joopy-KR@users.noreply.github.com>
2023-08-20 23:38:20 +0900

    [Bronze V] Title: A+B - 5, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -0,0 +1,9 @@
+while True:
+    try:
+        A, B = map(int, input().split())
+        if A == 0 and B == 0:
+            break
+        else:
+            print(A + B)
+    except:
+        break

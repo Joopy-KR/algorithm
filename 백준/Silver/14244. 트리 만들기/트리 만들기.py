80bebaa5f4beb358d495806cca9244d4738f6c59
김재형 <127040847+Joopy-KR@users.noreply.github.com>
2023-08-23 08:51:17 +0900

    [Silver IV] Title: 트리 만들기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14244. 트리 만들기/트리 만들기.py
+++ 백준/Silver/14244. 트리 만들기/트리 만들기.py
@@ -1,8 +1,4 @@
 N, M = map(int, input().split())
-
-for j in range(1, M + 1):
-    print(f'0 {j}')
-
+for j in range(1, M + 1): print(f'0 {j}')
 if (N - 1) != M:
-    for i in range(1, N - M):
-        print(f'{i} {i + M}')
+    for i in range(1, N - M): print(f'{i} {i + M}')

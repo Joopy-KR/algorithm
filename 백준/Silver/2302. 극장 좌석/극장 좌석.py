6f42225c4001827ebcd5e6da2328fcb9322d7c5c
김재형 <127040847+Joopy-KR@users.noreply.github.com>
2023-09-07 23:33:01 +0900

    [Silver I] Title: 극장 좌석, Time: 64 ms, Memory: 34176 KB -BaekjoonHub

---
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.py
@@ -0,0 +1,41 @@
+def solve(n):
+    if n == 0:
+        return 1
+    elif n == 1:
+        return 1
+    else:
+        f = [0] * (n + 1)
+        f[0] = 1
+        f[1] = 1
+        f[2] = 2
+        for i in range(3, n + 1):
+            f[i] = f[i - 1] + f[i - 2]
+        return f[n]
+
+
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+M = int(input().rstrip())
+fixed = deque()
+for _ in range(M):
+    fixed.append(int(input().rstrip()))
+
+# 고정석이 있을 때
+if M:
+    result = 1
+    start = 1
+    while fixed:
+        end = fixed.popleft()
+        result *= solve(end - start)
+        start = end + 1
+    if start != N:
+        end = N + 1
+        result *= solve(end - start)
+# 고정석이 없을 때
+else:
+    result = solve(N)
+
+print(result)

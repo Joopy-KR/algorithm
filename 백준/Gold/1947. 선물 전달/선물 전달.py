ae65fd23a5a4bb558732a4724ed4bfd5eaae1195
김재형 <127040847+Joopy-KR@users.noreply.github.com>
2023-10-31 19:38:33 +0900

    [Gold III] Title: 선물 전달, Time: 440 ms, Memory: 70676 KB -BaekjoonHub

--- 백준/Gold/1947. 선물 전달/선물 전달.py
+++ 백준/Gold/1947. 선물 전달/선물 전달.py
@@ -1,13 +1,14 @@
-import sys
-N = int(sys.stdin.readline())
-
-dp = [0] * (N+1)
+N = int(input())
 
 if N == 1:
     print(0)
 else:
+    dp = [0] * (N + 1)
+    dp[0] = 0
+    dp[1] = 0
     dp[2] = 1
-    for i in range(3, N+1):
-        dp[i] = ((dp[i-1]+dp[i-2]) * (i-1)) % 1000000000
+
+    for i in range(3, N + 1):
+        dp[i] = ((dp[i-2] + dp[i-1]) * (i - 1)) % 1000000000
 
     print(dp[N])

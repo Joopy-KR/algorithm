3cb8d4dc842f085dac467aa1624c2c27fe7d1a3e
김재형 <127040847+Joopy-KR@users.noreply.github.com>
2024-03-11 21:35:06 +0900

    [Bronze IV] Title: 상근날드, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+burger = [a, b, c]
+drink = [d, e]
+burger.sort()
+drink.sort()
+print(burger[0] + drink[0] - 50)

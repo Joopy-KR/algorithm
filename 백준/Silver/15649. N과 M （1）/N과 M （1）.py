e4167ea95900217d0672292bb0b671e1dedfd6bc
김재형 <127040847+Joopy-KR@users.noreply.github.com>
2024-11-18 21:39:33 +0900

    [Silver III] Title: N과 M (1), Time: 148 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -3,17 +3,21 @@ input = sys.stdin.readline
 
 
 def solution(depth, N, M, arr):
+    global visited
+
     if depth > M:
-        if len(arr) != len(set(arr)):
-            return
         print(" ".join(map(str, arr)))
         return
-    for i in range(N):
+    for i in range(1, N + 1):
         arr[depth - 1] += 1
-        solution(depth + 1, N, M, arr)
+        if not visited[i]:
+            visited[i] = True
+            solution(depth + 1, N, M, arr)
+            visited[i] = False
     arr[depth - 1] = 0
 
 
 N, M = map(int, input().rstrip().split())
 arr = [0] * M
+visited = [False] * (N + 1)
 solution(1, N, M, arr)

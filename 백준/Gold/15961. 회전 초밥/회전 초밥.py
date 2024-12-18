741df6a40c3bf4e735fd79a473d60716723eb209
김재형 <127040847+Joopy-KR@users.noreply.github.com>
2024-12-18 15:13:14 +0900

    [Gold IV] Title: 회전 초밥, Time: 3688 ms, Memory: 143268 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.py
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.py
@@ -1,11 +1,3 @@
-"""
-1. 우선 처음 k개의 경우를 생각한 뒤
-2. 왼쪽, 오른쪽 인덱스 잡고 하나씩 옮겨가며 오른쪽꺼 판단하기
-3. 매 이동시마다, 쿠폰이 포함되었는지 확인하기
-
-예외1. "회전" 초밥이므로 처음 k개 저장해뒀다가 나중에 포함해서 순회돌기
-예외2. 최대일 경우 쿠폰번호 포함되었는지 확인하기
-"""
 import sys
 input = sys.stdin.readline
 
@@ -15,13 +7,10 @@ sushis.extend(sushis[:k])
 sushi_counter = {i: 0 for i in range(1, d + 1)}
 now_cnt = 0
 max_cnt = -1
-is_coupon = False
 
 # 1. 우선 처음 k개의 경우를 생각한 뒤
 for i in range(k):
     sushi = sushis[i]
-    if sushi == c:
-        is_coupon = True
     sushi_counter[sushi] += 1
     if sushi_counter[sushi] == 1:
         now_cnt += 1

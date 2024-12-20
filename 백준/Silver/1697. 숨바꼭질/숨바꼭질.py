d05b59d3a8b8df561bc1c1a363fedce4765142eb
김재형 <127040847+Joopy-KR@users.noreply.github.com>
2024-12-20 10:38:53 +0900

    [Silver I] Title: 숨바꼭질, Time: 200 ms, Memory: 63356 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,29 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().rstrip().split())
+queue = deque()
+visited = [0] * 3000001
+queue.append(N)
+visited[N] = 1
+
+while queue:
+    now = queue.popleft()
+    # -1로 이동
+    if 0 < now:
+        if visited[now - 1] == 0:
+            queue.append(now - 1)
+            visited[now - 1] = visited[now] + 1
+    # +1로 이동
+    if now < (K * 2):
+        if visited[now + 1] == 0:
+            queue.append(now + 1)
+            visited[now + 1] = visited[now] + 1
+    # *2로 이동
+    if now <= K:
+        if visited[now * 2] == 0:
+            queue.append(now * 2)
+            visited[now * 2] = visited[now] + 1
+
+print(visited[K] - 1)

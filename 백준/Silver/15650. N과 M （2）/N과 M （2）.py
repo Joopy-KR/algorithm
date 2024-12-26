747c57dcf1fb08605675fe606f4ebfba8d71e4a5
김재형 <127040847+Joopy-KR@users.noreply.github.com>
2024-12-26 11:01:14 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,24 +1,11 @@
-def solution(n, m, seq, vst, now, before_min, min_num):
-    if now == m:
-        print(" ".join(map(str, seq)))
-        return
-    for i in range(1, n + 1):
-        if vst[i]:  # 중복되는 수열 출력하지 않음
-            continue
-        if min_num >= i:  # 오름차순 수열만 출력함
-            continue
-        before_min = min_num
-        min_num = i
-        vst[i] = True
-        seq[now] = i
-        solution(n, m, seq, vst, now + 1, before_min, min_num)
-        min_num = before_min
-        vst[i] = False
-
-
-import sys
-input = sys.stdin.readline
-N, M = map(int, input().rstrip().split())
-sequence = [0] * M
-visited = [False] * (N + 1)
-solution(N, M, sequence, visited, 0, 0, 0)
+import sys; input = sys.stdin.readline
+N, M = map(int, input().split())
+stack = []
+def solution(now):
+    if len(stack) == M:
+        print(" ".join(map(str, stack))); return
+    for i in range(now, N + 1):
+        stack.append(i)
+        solution(i + 1)
+        stack.pop()
+solution(1)

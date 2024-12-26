914c9bf2e0db6b397acccedecfd6d59e56f4ad75
김재형 <127040847+Joopy-KR@users.noreply.github.com>
2024-12-26 11:03:00 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,11 +1,6 @@
-import sys; input = sys.stdin.readline
 N, M = map(int, input().split())
-stack = []
-def solution(now):
-    if len(stack) == M:
-        print(" ".join(map(str, stack))); return
+def solution(stack, now):
+    if len(stack) == M: print(" ".join(map(str, stack))); return
     for i in range(now, N + 1):
-        stack.append(i)
-        solution(i + 1)
-        stack.pop()
-solution(1)
+        stack.append(i); solution(stack, i + 1); stack.pop()
+solution([], 1)

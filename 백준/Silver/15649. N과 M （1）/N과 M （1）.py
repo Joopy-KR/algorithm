e47935609b4a526e0e63fb453c7393696a050040
김재형 <127040847+Joopy-KR@users.noreply.github.com>
2024-12-26 10:15:38 +0900

    [Silver III] Title: N과 M (1), Time: 128 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,23 +1,21 @@
+# 재귀의 시작은 종료조건 설정부터
+def solution(n, m, seq, vst, now):
+    if now == m:
+        print(" ".join(map(str, seq)))
+        return
+    for i in range(1, n + 1):
+        if vst[i]:
+            continue
+        seq[now] = i
+        vst[i] = True
+        solution(n, m, seq, vst, now + 1)
+        vst[i] = False
+
+
 import sys
 input = sys.stdin.readline
 
-
-def solution(depth, N, M, arr):
-    global visited
-
-    if depth > M:
-        print(" ".join(map(str, arr)))
-        return
-    for i in range(1, N + 1):
-        arr[depth - 1] += 1
-        if not visited[i]:
-            visited[i] = True
-            solution(depth + 1, N, M, arr)
-            visited[i] = False
-    arr[depth - 1] = 0
-
-
-N, M = map(int, input().rstrip().split())
-arr = [0] * M
-visited = [False] * (N + 1)
-solution(1, N, M, arr)
+N, M = map(int, input().split())
+sequence = [0] * M
+visited = {i:False for i in range(1, N + 1)}
+solution(N, M, sequence, visited, 0)

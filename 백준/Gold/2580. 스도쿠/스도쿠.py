89480889c869dc87f6c657b1c0129c79da284904
김재형 <127040847+Joopy-KR@users.noreply.github.com>
2024-12-31 14:29:00 +0900

    [Gold IV] Title: 스도쿠, Time: 3024 ms, Memory: 140096 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -1,51 +1,53 @@
-import sys
-input = sys.stdin.readline
-
-arr = [list(map(int, input().rstrip().split())) for _ in range(9)]
-blank = []
-
-for n in range(9):
-    for m in range(9):
-        if arr[n][m] == 0:
-            blank.append([n, m])
+def get_blank(arr: list) -> list:
+    temp_blank = []
+    for r in range(9):
+        for c in range(9):
+            if arr[r][c] == 0:
+                temp_blank.append([r, c])
+    return temp_blank
 
 
-def is_row(r, num):
+def check_row(r, num, arr):
     for c in range(9):
         if arr[r][c] == num:
             return False
     return True
 
 
-def is_col(c, num):
+def check_col(c, num, arr):
     for r in range(9):
         if arr[r][c] == num:
             return False
     return True
 
 
-def is_box(r, c, num):
-    nr = (r // 3) * 3
-    nc = (c // 3) * 3
-    for i in range(3):
-         for j in range(3):
-             if arr[nr + i][nc + j] == num:
-                 return False
+def check_box(r, c, num, arr):
+    r = (r // 3) * 3
+    c = (c // 3) * 3
+    for n in range(3):
+        for m in range(3):
+            if arr[r + n][c + m] == num:
+                return False
     return True
 
 
-def solution(idx):
-    if idx == len(blank):
+def solution(arr: list, blk: list, now: int):
+    if now == len(blk):
         for i in range(9):
             print(*arr[i])
         exit(0)
 
-    for j in range(1, 10):
-        x, y = blank[idx]
-        if is_row(x, j) and is_col(y, j) and is_box(x, y, j):
-            arr[x][y] = j
-            solution(idx + 1)
-            arr[x][y] = 0
+    for num in range(1, 10):
+        r, c = blk[now]
+        if check_row(r, num, arr) and check_col(c, num, arr) and check_box(r, c, num, arr):
+            arr[r][c] = num
+            solution(arr, blk, now + 1)
+            arr[r][c] = 0
 
 
-solution(0)
+import sys
+input = sys.stdin.readline
+
+array = [list(map(int, input().rstrip().split())) for _ in range(9)]
+blank = get_blank(array)
+solution(array, blank, 0)
